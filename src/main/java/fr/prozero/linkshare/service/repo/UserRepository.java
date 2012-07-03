08cada1b5b75d342e5bd6580ebf8ade7cbd23fa9
Aure77 <aure77+github@gmail.com>
2012-07-03 20:23:50 +0200

    Correction nom champ

--- src/main/java/fr/prozero/linkshare/service/repo/UserRepository.java
+++ src/main/java/fr/prozero/linkshare/service/repo/UserRepository.java
@@ -7,7 +7,7 @@ import fr.prozero.linkshare.model.LsUser;
 
 public interface UserRepository extends JpaRepository<LsUser, Long> {
 
-	@Query("From LsUser u WHERE u.email = ?1")
-	public LsUser findUserByEmail(String username);
+	@Query("From LsUser u WHERE u.userEmail = ?1")
+	public LsUser findUserByEmail(String email);
 	
 }
