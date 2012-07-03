7f1bbb11d5bfdf9103396740a85407c433c2c0ee
Aure77 <aure77+github@gmail.com>
2012-07-03 23:03:35 +0200

    @Transactional

--- src/main/java/fr/prozero/linkshare/service/repo/UserRepository.java
+++ src/main/java/fr/prozero/linkshare/service/repo/UserRepository.java
@@ -2,9 +2,11 @@ package fr.prozero.linkshare.service.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.prozero.linkshare.model.LsUser;
 
+@Transactional
 public interface UserRepository extends JpaRepository<LsUser, Long> {
 
 	@Query("From LsUser u WHERE u.userEmail = ?1")
